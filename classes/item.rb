44e5f05fd48d0e65eb08f1665c9fd324cbc37384
sarazaToussaint <tmsaraza@gmail.com>
2022-08-09 12:24:28 +0200

    Add relationship between label and item classes

--- classes/item.rb
+++ classes/item.rb
@@ -20,6 +20,11 @@ class Item
     author.items.push(self) unless author.items.include?(self)
   end
 
+  def add_label(label)
+    @label = label
+    label.items.push(self) unless label.items.include?(self)
+  end
+
   def move_to_archive
     true if can_be_archived?
   end
