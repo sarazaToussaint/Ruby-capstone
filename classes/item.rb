ec3032f5888c05227745c475261991102f13dddd
unknown <ibr.s.ahmat@gmail.com>
2022-08-09 12:59:01 +0300

    Add Item class

---
+++ classes/item.rb
@@ -0,0 +1,20 @@
+require 'date'
+
+class Item
+  attr_accessor :publish_date
+  attr_reader :genre, :label, :author, :archived, :source
+
+  def initialize(date)
+    @id = Random.rand(1..1000)
+    @publish_date = Date.strptime(date, '%d-%m-%Y')
+    @archived = false
+  end
+
+  def move_to_archive
+    true if can_be_archived?
+  end
+
+  def can_be_archived?
+    (Date.today.year - @publish_date.year) > 10
+  end
+end
