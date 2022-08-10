191417df07b83d7b38dfa23b60cbf74bbf4cc7de
unknown <ibr.s.ahmat@gmail.com>
2022-08-10 12:19:22 +0300

    Create Genre class

---
+++ classes/genre.rb
@@ -0,0 +1,16 @@
+require_relative 'item'
+
+class Genre
+  attr_reader :name, :items
+
+  def initialize(name)
+    @id = Random.rand(1..1000)
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items.push(item)
+    item.add_label(self)
+  end
+end
